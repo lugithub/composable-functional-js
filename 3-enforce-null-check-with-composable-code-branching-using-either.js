6309470bbc75f19b3f9a4bfae02fe34f6a80a2bd
bin lu <lu.bin.lu.bin@gmail.com>
2016-12-12 16:35:42 -0500

    enforce null check with composable code brancing using either

---
+++ 3-enforce-null-check-with-composable-code-branching-using-either.js
@@ -0,0 +1,24 @@
+const Right = x => ({
+  map: f => Right(f(x)),
+  fold: (f, g) => g(x),
+  inspect: () => `Right(${x})`,
+});
+
+const Left = x => ({
+  map: f => Left(x),
+  fold: (f, g) => f(x),
+  inspect: () => `Left(${x})`,
+});
+
+//fromNullable returns Either
+const fromNullable = x =>
+  x != null ? Right(x) : Left(x);
+
+const findColor = name =>
+    fromNullable({red: '#ff4444', blue: '#3b5998', yellow: '#fff68f'}[name]);
+
+const result = findColor('red1')
+  .map(c => c.slice(1))
+  .fold(() => `no color` , c => c.toUpperCase());
+
+console.log(result);
